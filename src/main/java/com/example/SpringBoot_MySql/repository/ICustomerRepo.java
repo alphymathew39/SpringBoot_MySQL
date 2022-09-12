6d13886f84a26a4a94841f601de9039c22affb69
Alphy <alphymathew020@gmail.com>
2022-09-12 17:18:35 +0530

    SpringBoot and MySQL

---
+++ src/main/java/com/example/SpringBoot_MySql/repository/ICustomerRepo.java
@@ -0,0 +1,11 @@
+package com.example.SpringBoot_MySql.repository;
+
+import com.example.SpringBoot_MySql.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ICustomerRepo extends JpaRepository<Customer, Long> {
+
+}
---
+++ src/test/java/com/example/SpringBoot_MySql/SpringBootMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_MySql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
